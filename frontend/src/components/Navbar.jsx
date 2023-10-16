92b549810228fb94ca13865cadc955780361f6d8
habfem <habeebmustapha96@gmail.com>
2023-10-16 05:19:33 -0500

    update

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,252 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+//import { Search } from "@material-ui/icons";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import { logout } from "../redux/userRedux";
+import { Typography, Stack, Container, Button, Drawer } from "@mui/material";
+import NavBarDrawer from "./NavBarDrawer";
+import MobileNavBar from "./MobileNavBar";
+
+export const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+    margin-left: 10px;
+  }
+`;
+const Navbar = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [drawer, setDrawer] = useState(false);
+
+  const openDrawer = () => {
+    setDrawer(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawer(false);
+  };
+
+  const signup = () => {
+    navigate("/signup");
+  };
+
+  const signin = () => {
+    navigate("/login");
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
+  const homePage = () => {
+    navigate("/");
+  };
+
+  return (
+    <Container>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        py={2.5}
+      >
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <Typography variant="h5" letterSpacing="2px" color="teal">
+            Romax
+          </Typography>
+        </Link>
+
+        <MobileNavBar openDrawer={openDrawer} />
+        <Stack
+          direction="row"
+          spacing={3.5}
+          display={{ xs: "none", md: "flex" }}
+        >
+          <Link
+            to="/products"
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <Typography
+              color="#2b3445"
+              variant="body2"
+              sx={{
+                "&:hover": {
+                  color: "teal",
+                },
+              }}
+            >
+              House Prices
+            </Typography>
+          </Link>
+          <Link
+            to="/estate"
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <Typography
+              color="#2b3445"
+              variant="body2"
+              sx={{
+                "&:hover": {
+                  color: "teal",
+                },
+              }}
+            >
+              Estate Prices
+            </Typography>
+          </Link>
+          <Link
+            to="/about"
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <Typography
+              color="#2b3445"
+              variant="body2"
+              sx={{
+                "&:hover": {
+                  color: "teal",
+                },
+              }}
+            >
+              Our Timeline
+            </Typography>
+          </Link>
+          <Link
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <Typography
+              color="#2b3445"
+              variant="body2"
+              sx={{
+                "&:hover": {
+                  color: "teal",
+                },
+              }}
+            >
+              Instant Valuation
+            </Typography>
+          </Link>
+        </Stack>
+
+        <Stack
+          direction="row"
+          spacing={3.5}
+          alignItems="center"
+          display={{ xs: "none", md: "flex" }}
+        >
+          {user && (
+            <Link
+              to="/user/profile"
+              style={{
+                textDecoration: "none",
+              }}
+            >
+              <Typography
+                color="#2b3445"
+                variant="body2"
+                sx={{
+                  "&:hover": {
+                    color: "teal",
+                  },
+                }}
+              >
+                {`Hi,${user.username}`}
+              </Typography>
+            </Link>
+          )}
+          <Link
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <Typography
+              color="#2b3445"
+              variant="body2"
+              sx={{
+                "&:hover": {
+                  color: "teal",
+                },
+              }}
+            >
+              Saved
+            </Typography>
+          </Link>
+          {user ? (
+            <Button
+              onClick={handleLogout}
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                color: "teal",
+
+                borderRadius: "8px",
+                borderColor: "teal",
+                borderWidth: "2px",
+                "&:hover": {
+                  color: "#FFFFFF",
+                  bgcolor: "teal",
+                },
+              }}
+            >
+              <Typography variant="body2"> Logout</Typography>
+            </Button>
+          ) : (
+            <Button
+              onClick={signin}
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                color: "teal",
+
+                borderRadius: "8px",
+                borderColor: "teal",
+                borderWidth: "2px",
+                "&:hover": {
+                  color: "#FFFFFF",
+                  bgcolor: "teal",
+                },
+              }}
+            >
+              <Typography variant="body2"> Sign in</Typography>
+            </Button>
+          )}
+        </Stack>
+      </Stack>
+      <Drawer
+        open={drawer}
+        onClose={closeDrawer}
+        anchor="left"
+        bgcolor="white"
+        sx={{
+          zIndex: "1200",
+          "& .MuiPaper-root": {
+            backgroundColor: "white",
+          },
+        }}
+      >
+        <NavBarDrawer />
+      </Drawer>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Carousel from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Facilities from "../components/Facilities";
+import { resetState } from "../redux/filter";
+import { useDispatch } from "react-redux";
+
+const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(resetState());
+  }, []);
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Hero />
+      <Categories />
+      <Carousel />
+      <br />
+      <Products />
+      <br />
+      <Facilities />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
