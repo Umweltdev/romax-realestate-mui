d8c9ba1d6fbae71ef9f51754bfd5d7a1d6811a34
seunfola <seunfola1@gmail.com>
2025-06-26 11:56:27 +0100

    removed about from timeline and fixed the timeline page properly

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,14 +4,12 @@ import Footer from '../components/Footer'
 import Timelines from '../components/Timeline'
 import Announcement from '../components/Announcement'
 import Newsletter from '../components/Newsletter'
-import AboutHero from '../components/AboutHero'
 
 const About = () => {
   return (
     <>
       <Announcement />
       <Navbar />
-      <AboutHero />
       <Timelines />
       <Newsletter />
       <Footer />
